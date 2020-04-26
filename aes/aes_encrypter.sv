2b5460f8d607986c903693b020e65d6fcaaaea0f
arielk <arielk@FPGAserver.pvombxvgjv2e1pl0sgxcqkhaxg.ax.internal.cloudapp.net>
2020-04-26 13:04:03 +0000

    add dvr and change state machine

--- aes/aes_encrypter.sv
+++ aes/aes_encrypter.sv
@@ -1,8 +1,8 @@
 //////////////////////////////////////////////////////////////////
 ///
-/// Project Name: 	avalon_enforcer
+/// Project Name: 	aes_encrypter
 ///
-/// File Name: 		avalon_enforcer.sv
+/// File Name: 		aes_encrypter.sv
 ///
 //////////////////////////////////////////////////////////////////
 ///
@@ -17,3 +17,32 @@
 /// Description:    the model will encrypt message using AES128
 ///
 //////////////////////////////////////////////////////////////////
+module avalon_enforcer
+#(
+	parameter int DATA_WIDTH_IN_BYTES = 16,
+	parameter int BLOCK_SIZE = 128,
+
+)
+(
+	input logic             clk,
+	input logic             rst,
+
+	avalon_st_if.slave      avalon_st_in,
+	avalon_st_if.master     avalon_st_out,
+
+	dvr_if.slave            key_and_sync_in,
+
+	output logic            sync_error
+
+);
+
+
+//////////////////////////////////////////
+//// Typedefs ////////////////////////////
+//////////////////////////////////////////
+
+typedef enum logic {
+		WAITING_FOR_KEY,
+		GENERATE_SYNC,
+		ENCRYPT_MSG
+	} manage_sm_t;
